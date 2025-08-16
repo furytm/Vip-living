f8f2f7059afb3422681d9d30f45ae6b3eaae149c
furytm <kanuemma367@gmail.com>
2025-08-16 11:28:33 +0100

    improve logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
 
 	return (
 		<header className="fixed top-0 w-full bg-white/95 backdrop-blur-sm border-b border-slate-200 z-50">
-			<div className="container mx-auto px-4 py-4">
+			<div className="container mx-auto px-2 py-4">
 				<div className="flex items-center justify-between">
 					<motion.div
 						initial={{ opacity: 0, x: -20 }}
@@ -29,13 +29,15 @@ export function Header() {
 							to="/"
 							className="flex items-center space-x-2">
 							<img
-								src="/logo.png"
+								src="/logo.jpg"
 								alt="logo"
-								className="h-5"
+								className="h-12 "
 							/>
-							<h1 className="text-md font-playwrite text-slate-900 font-bold">
-							VIP LIVING <br></br> CENTER
-							</h1>
+							<div className="max-w-[200px]">
+								<h1 className=" text-[10px] lg:text-xs font-playread text-slate-900 font-normal">
+							VIP LIVING CENTER <br></br> A Home away from Home
+							</h1></div>
+							
 							{/* <div c
 							{/* <div className="w-10 h-10 bg-gradient-to-br from-emerald-500 to-teal-600 rounded-lg flex items-center justify-center">
 								<Home className="w-6 h-6 text-white" />
